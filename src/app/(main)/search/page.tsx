b612c38f724764ee69b981f921111f418bff73ac
joearias1121 <joearias1121@gmail.com>
2025-06-09 07:34:29 -0400

    added user location and adding range of search

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+//import {useState} from "react";
 import SearchInput from "@/components/SearchInput";
 import { Card } from "@/components/ui/card";
 import GoogleMap from "@/components/GoogleMap";
+import { Slider } from "@/components/ui/slider";
 
 export default function Search() {
+  //const [distance, setDistance] = useState([0]);
+  // <Slider value={distance} />
   return (
     <>
       <h1>Search</h1>
-      <div className="flex flex-col w-1/4">
+      <div className="flex w-1/4 flex-col">
         <SearchInput />
         <GoogleMap />
       </div>
--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,16 +1,38 @@
 "use client";
+import { useEffect, useState } from "react";
 import { AdvancedMarker, APIProvider, Map } from "@vis.gl/react-google-maps";
 
 export default function GoogleMap() {
-  const position = { lat: 61.2176, lng: -149.8997 };
+  const [location, setLocation] = useState<{lat: number, lng: number} | null>(null)
+  
+  useEffect(() => {
+    // Get the current location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      }, (error) => {
+        console.error("Error getting location:", error);
+      });
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, [])
+  // TODO: use toasts and/or loading state to if error or loading
+  // this is temporary
+  if (!location) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <APIProvider
-      className="h-full"
       apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}
     >
       <div className="flex h-80">
-        <Map className="h-full w-full" center={position} zoom={10} mapId={"f4c0b1d3a2b6c5e7"}>
-          <AdvancedMarker position={position} />
+        <Map className="h-full w-full" center={location} zoom={10} mapId={process.env.NEXT_PUBLIC_MAP_ID as string}>
+          <AdvancedMarker position={location} />
         </Map>
       </div>
     </APIProvider>
